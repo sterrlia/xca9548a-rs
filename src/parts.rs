9b27ce25ab0cbc1f0e95590a55f8f1c889c05685
sterrlia <dev@sterrlia.com>
2026-08-29 12:56:01 +0400

    separate methods for i2c slaves to select and deselect their channels, disable autoselect

--- src/parts.rs
+++ src/parts.rs
@@ -46,6 +46,33 @@ where
     type Error = Error<E>;
 }
 
+impl<'a, DEV, I2C, E> I2cSlave<'a, DEV, I2C>
+where
+    DEV: DoOnAcquired<I2C>,
+    I2C: ehal::I2c<Error = E>,
+    E: ehal::Error,
+{
+    /// enable i2c slave channel
+    pub fn select_channel(&mut self) -> Result<(), Error<E>> {
+        self.0.do_on_acquired(|mut dev| {
+            let new_mask = dev.selected_channel_mask | self.1;
+            dev.select_channels(new_mask)?;
+
+            Ok(())
+        })
+    }
+
+    /// disable i2c slave channel
+    pub fn deselect_channel(&mut self) -> Result<(), Error<E>> {
+        self.0.do_on_acquired(|mut dev| {
+            let new_mask = dev.selected_channel_mask & !self.1;
+            dev.select_channels(new_mask)?;
+
+            Ok(())
+        })
+    }
+}
+
 impl<'a, DEV, I2C, E> ehal::I2c for I2cSlave<'a, DEV, I2C>
 where
     DEV: DoOnAcquired<I2C>,
@@ -57,30 +84,18 @@ where
         address: u8,
         operations: &mut [ehal::Operation<'_>],
     ) -> Result<(), Self::Error> {
-        self.0.do_on_acquired(|mut dev| {
-            if dev.selected_channel_mask != self.1 {
-                dev.select_channels(self.1)?;
-            }
-            dev.i2c.transaction(address, operations).map_err(Error::I2C)
-        })
+        self.0
+            .do_on_acquired(|mut dev| dev.i2c.transaction(address, operations).map_err(Error::I2C))
     }
 
     fn read(&mut self, address: u8, read: &mut [u8]) -> Result<(), Self::Error> {
-        self.0.do_on_acquired(|mut dev| {
-            if dev.selected_channel_mask != self.1 {
-                dev.select_channels(self.1)?;
-            }
-            dev.i2c.read(address, read).map_err(Error::I2C)
-        })
+        self.0
+            .do_on_acquired(|mut dev| dev.i2c.read(address, read).map_err(Error::I2C))
     }
 
     fn write(&mut self, address: u8, write: &[u8]) -> Result<(), Self::Error> {
-        self.0.do_on_acquired(|mut dev| {
-            if dev.selected_channel_mask != self.1 {
-                dev.select_channels(self.1)?;
-            }
-            dev.i2c.write(address, write).map_err(Error::I2C)
-        })
+        self.0
+            .do_on_acquired(|mut dev| dev.i2c.write(address, write).map_err(Error::I2C))
     }
 
     fn write_read(
@@ -89,11 +104,7 @@ where
         write: &[u8],
         read: &mut [u8],
     ) -> Result<(), Self::Error> {
-        self.0.do_on_acquired(|mut dev| {
-            if dev.selected_channel_mask != self.1 {
-                dev.select_channels(self.1)?;
-            }
-            dev.i2c.write_read(address, write, read).map_err(Error::I2C)
-        })
+        self.0
+            .do_on_acquired(|mut dev| dev.i2c.write_read(address, write, read).map_err(Error::I2C))
     }
 }
